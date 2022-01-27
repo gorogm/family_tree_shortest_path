fe4db1265d5b3545704131eea3e4a8f4f54b2ae8
Gorog Marton <gorog@mukkozpont.hu>
2022-01-27 22:57:36 +0100

    2nd commit

---
+++ shortest_connection.py
@@ -0,0 +1,86 @@
+from gedcom.parser import Parser
+import streamlit as st
+
+from utils import *
+import pickle
+
+st.set_page_config(layout="wide")
+
+import gm_secrets
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == gm_secrets.password:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+
+def main():
+    st.title('Legrövidebb út két ember között a Hollai/Görög családfában')
+    st.write('Add meg a két személy nevét, majd kattints a gombra. Név-töredék is megadható, viszont kisbetű-nagybetű érzékeny. Az évszám nem kötelező, de segíthet szűkíteni. 1800')
+
+    col1, col2, col3, col4 = st.columns(4)
+    p1_1 = col1.text_input('1. személy vezetéknév:', value="Görög")
+    p1_2 = col2.text_input('1. személy keresztnév:', value="Márton Jenő")
+    p1_filter_birth = col3.checkbox('Szűrés születési évre', key='p1_filter_birth')
+    if p1_filter_birth:
+        p1_birth = col4.number_input('1. személy születési év:', min_value=1600, max_value=2022, format='%d')
+    col1, col2, col3, col4 = st.columns(4)
+    p2_1 = col1.text_input('2. személy vezetéknév:', value="Andrássy")
+    p2_2 = col2.text_input('2. személy keresztnév:', value="Ilona")
+    p2_filter_birth = col3.checkbox('Szűrés születési évre', key='p2_filter_birth')
+    if p2_filter_birth:
+        p2_birth = col4.number_input('1. személy születési év:', min_value=1600, max_value=2022, format='%d')
+
+    if st.button('Keress'):
+        try:
+            gedcom_parser
+        except NameError:
+            st.write('Családfa betöltése (~30 másodperc)...')
+            with open('gedcom_parser.bin', 'rb') as handle:
+                gedcom_parser = pickle.load(handle)
+
+        root_child_elements = gedcom_parser.get_root_child_elements()
+
+        p1 = find(root_child_elements, p1_1, p1_2, p1_birth if p1_filter_birth else None)
+        if p1 is None:
+            st.error(p1_1 + ' ' + p1_2 + ' nem található')
+            return
+        p2 = find(root_child_elements, p2_1, p2_2, p2_birth if p2_filter_birth else None)
+        if p2 is None:
+            st.error(p2_1 + ' ' + p2_2 + ' nem található')
+            return
+
+        st.write('Kapcsolat keresése...')
+        jumps = findConnection(gedcom_parser, p1, p2)
+
+        st.write('Kirajzolás...')
+        img = drawJumps(gedcom_parser, p2, jumps)
+
+        st.image(img.render())
+    
+
+if check_password():
+    main()
---
+++ utils.py
@@ -0,0 +1,97 @@
+from gedcom.element.individual import IndividualElement
+from graphviz import Digraph
+
+def find(root_child_elements, family_name, given_name, birthyear=None):
+    for element in root_child_elements:
+        if isinstance(element, IndividualElement):
+            (first, last) = element.get_name()
+            if family_name in last and given_name in first:
+                if birthyear is None:
+                    return element
+                if element.get_birth_year() == -1 or element.get_birth_year() == birthyear:
+                    return element
+    return None
+                    
+def toString(p):
+    string = p.get_name()[1] + ' ' + p.get_name()[0]
+    birthyear = p.get_birth_year()
+    deathyear = p.get_death_year()
+
+    return string + ' (' + (str(birthyear) if birthyear > 0 else '') + ' - ' + (str(deathyear) if deathyear > 0 else '') + ')'
+
+def getRelation(gedcom_parser, current, new_current):
+    if new_current in gedcom_parser.get_parents(current):
+        if current.get_gender() == 'M':
+            return ' son of '
+        elif current.get_gender() == 'F':
+            return ' daughter of '
+        else:
+            return ' child of '
+    elif current in gedcom_parser.get_parents(new_current):
+        if current.get_gender() == 'M':
+            return ' father of '
+        elif current.get_gender() == 'F':
+            return ' mother of '
+        else:
+            return ' parent of '
+    else:
+        return ' spouse of '
+
+def getRelationDirection(gedcom_parser, current, new_current):
+    if new_current in gedcom_parser.get_parents(current):
+        return 1
+    elif current in gedcom_parser.get_parents(new_current):
+        return -1
+    else:
+        return 0
+
+def findConnection(gedcom_parser, p1, p2):
+    visited = set([p1])
+    leaves = set([p1])
+    jumps = dict()
+    while len(leaves) > 0:
+        leaves_this_round = leaves.copy()
+        leaves = set()
+        for l in leaves_this_round:
+            families = gedcom_parser.get_families(l)
+            for f in families:
+                for p in gedcom_parser.get_family_members(f):
+                    if p not in visited:
+                        jumps[p] = l
+                        if p == p2:
+                            print('found')
+                            return jumps
+                        visited.add(p)
+                        leaves.add(p)
+            for p in gedcom_parser.get_parents(l):
+                if p not in visited:
+                    jumps[p] = l
+                    if p == p2:
+                        print('found')
+                        return jumps
+                    visited.add(p)
+                    leaves.add(p)
+    return jumps
+
+def drawJumps(gedcom_parser, target, jumps):
+    f = Digraph('connection', format='jpg', encoding='utf8', filename='connection', node_attr={'style': 'filled'},  graph_attr={"concentrate": "true", "splines":"ortho"})
+    f.attr('node', shape='box')
+
+    current = target
+    i = -1
+    while current in jumps:
+        i+=1
+        new_current = jumps[current]
+        f.node(str(i), label = toString(current), _attributes={'color':'lightpink' if current.get_gender()=='F' else 'lightblue'if current.get_gender()=='M' else 'lightgray'})
+        if getRelationDirection(gedcom_parser, current, new_current)  == 1:
+            f.edge(str(i+1), str(i), label='')
+        if getRelationDirection(gedcom_parser, current, new_current)  == -1:
+            f.edge(str(i), str(i+1), label='')
+        if getRelationDirection(gedcom_parser, current, new_current)  == 0:
+            f.edge(str(i), str(i+1), label='', constraint='False')
+        current = new_current
+
+    i+=1
+    f.node(str(i), label = toString(current), _attributes={'color':'lightpink' if current.get_gender()=='F' else 'lightblue'if current.get_gender()=='M' else 'lightgray'})
+
+    return f
